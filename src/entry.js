d82163f0c8a7dc2a4926b7ad13f82dfef89264a8
Alberto Casero <alberto@kasfactory.net>
2018-02-27 22:08:22 +0100

    Refactorizamos el código para que sea mucho más legible

--- src/entry.js
+++ src/entry.js
@@ -1,33 +1,9 @@
 import css from './scss/style.scss';
-import { SongsService } from './js/SongsService';
+import { SongsListController } from './js/SongsListController';
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    let songsService = new SongsService();
-
-    document.querySelector(".songs-list").innerHTML = '<div class="loading">Cargando...</div>';
-
-    songsService.list().then(songs => {
-        let html = "";
-        if (songs.length == 0) {
-            html = '<div class="info">No hay ninguna canción</div>';
-        } else {
-            for (let song of songs) {
-                html += `<article class="song">
-                    <div class="cover">
-                        <img src="${song.cover}" alt="${song.artist} - ${song.title}">
-                    </div>
-                    <div class="info">
-                        <div class="title">${song.title}</div>
-                        <div class="artist">${song.artist}</div>
-                    </div>
-                </article>`;
-            }
-        }
-        document.querySelector(".songs-list").innerHTML = html;
-    }).catch((error) => {
-        console.error("ERROR RETRIEVING SONGS", error);
-        document.querySelector(".songs-list").innerHTML = '<div class="error">Se ha producido un error</div>';
-    });
+    let songsListController = new SongsListController(".songs-list");
+    songsListController.loadSongs();
 
 });
